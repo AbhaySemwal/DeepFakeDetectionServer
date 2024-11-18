b668edfc9adfddd4b23d9fe151a181edd6ed093b
Abhay Semwal <shbhtsemwal@gmail.com>
2024-11-18 22:36:32 +0530

    Update server.py

--- server.py
+++ server.py
@@ -122,4 +122,5 @@ def DetectPage():
     return jsonify({'output': output, 'confidence': confidence})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
